ed386314c6463220363b42e21597dc37971c2b4f
Alexis <alexis.73400@icloud.com>
2022-01-02 16:32:02 +0100

    ajout du formulaire de la modal

--- src/pages/Home/Content/Modal.tsx
+++ src/pages/Home/Content/Modal.tsx
@@ -19,6 +19,15 @@ export default function Modal({ displayModal }: ModalProps) {
           e.stopPropagation()
         }}
       >
+        <img
+          className='closeButton'
+          src='https://static.xx.fbcdn.net/rsrc.php/v3/y2/r/__geKiQnSG-.png'
+          alt='close signup modal'
+          role='button'
+          onClick={(e) => {
+            handleBlur(e)
+          }}
+        />
         <h2>S'inscrire</h2>
         <p>C'est rapide et facile.</p>
         <div className='separator'></div>
@@ -50,7 +59,20 @@ export default function Modal({ displayModal }: ModalProps) {
             id='password'
           />
 
+          <p>
+            En cliquant sur S’inscrire, vous acceptez nos{' '}
+            <span>Conditions générales.</span>
+            Découvrez comment nous recueillons, utilisons et partageons vos
+            données en lisant notre{' '}
+            <span>Politique d’utilisation des données</span> et comment nous
+            utilisons les cookies et autres technologies similaires en
+            consultant notre <span>Politique d’utilisation des cookies.</span>
+            Vous recevrez peut-être des notifications par texto de notre part et
+            vous pouvez à tout moment vous désabonner.
+          </p>
+
           {/* TODO Finir le formulaire ! */}
+          <button role='submit'>S'inscrire</button>
         </Form>
       </Signup>
     </StyledBackground>
--- src/pages/Home/Content/styled/Modal.styled.ts
+++ src/pages/Home/Content/styled/Modal.styled.ts
@@ -12,14 +12,23 @@ export const StyledBackground = styled.div`
 export const Signup = styled.div`
   box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
   width: 450px;
-  height: 550px;
+  height: 450px;
   background-color: white;
   border-radius: 10px;
+  padding: 5px;
+  position: relative;
 
   .separator {
     height: 1px;
     width: 100%;
-    background-color: grey;
+    background-color: #dadde1;
+  }
+
+  .closeButton {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    cursor: pointer;
   }
 
   h2 {
@@ -51,5 +60,30 @@ export const Form = styled.form`
     background-color: #f5f6f7;
     border: 1px solid #e2e2e2;
     border-radius: 5px;
+  }
+
+  p {
+    font-size: 11px;
+    color: #777;
+    padding: 0;
+    line-height: 15px;
+
+    span {
+      color: #385898;
+      cursor: pointer;
+    }
+  }
+
+  button {
+    margin: 0 auto;
+    margin-top: 20px;
+    padding: 0 45px;
+    height: 36px;
+    font-size: 18px;
+    background-color: #00a400;
+    color: white;
+    font-weight: bold;
+    border-color: transparent;
+    border-radius: 8px;
   }
 `
